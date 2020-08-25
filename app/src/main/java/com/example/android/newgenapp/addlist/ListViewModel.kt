9cbca4fe8d0032e883c994698f65e656d1f978e9
Jane02294 <46182833+Jane02294@users.noreply.github.com>
2020-08-25 16:09:22 +0700

    Add ListViewModel

--- app/src/main/java/com/example/android/newgenapp/addlist/ListViewModel.kt
+++ app/src/main/java/com/example/android/newgenapp/addlist/ListViewModel.kt
@@ -1,4 +1,9 @@
 package com.example.android.newgenapp.addlist
 
-class ListViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class ListViewModel(val database: ListDatabase,
+                    application: Application
+) :AndroidViewModel(application) {
 }
