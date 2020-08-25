c87fa90ce261cd07f120a13b7704c9941ccf9769
Jane02294 <46182833+Jane02294@users.noreply.github.com>
2020-08-26 01:26:18 +0700

    Add xml

--- app/src/main/java/com/example/android/newgenapp/addlist/ListViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.android.newgenapp.addlist
-
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-
-class ListViewModel(val database: ListDatabase,
-                    application: Application
-) :AndroidViewModel(application) {
-}
