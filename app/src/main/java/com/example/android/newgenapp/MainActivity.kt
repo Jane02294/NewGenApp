98e5e980d2de3d57e586201f2daae787e8901245
Jane02294 <46182833+Jane02294@users.noreply.github.com>
2020-08-25 12:11:09 +0700

    Modify activity_main.xml & MainActivity.kt

--- app/src/main/java/com/example/android/newgenapp/MainActivity.kt
+++ app/src/main/java/com/example/android/newgenapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.android.newgenapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.android.newgenapp.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var drawer_layout: DrawerLayout
+    private lateinit var navigation_view: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        drawer_layout = binding.drawerlayout
+        navigation_view = binding.navView
+
     }
 }
